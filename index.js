aea068bc98152873e21674850ad1bc4498702e76
Paul Starkebaum <paulstarkebaum@gmail.com>
2022-09-15 14:57:56 -0300

    Initialize repository

---
+++ index.js
@@ -0,0 +1,42 @@
+// // Reverses a palindrome
+// function reverse(string) {
+// 	return Array.from(string).reverse().join("")
+// }
+String.prototype.reverse = function () {
+	return Array.from(this).reverse().join("");
+}
+String.prototype.blank = function() {
+	return !!this.match(/^\s+$/);
+};
+Array.prototype.last = function() {
+	return this.slice(-1);
+}
+// Defines a Phrase object,
+function Phrase(content) {	
+	this.content = content;
+
+	this.processor = function (string) {
+		return string.toLowerCase()
+	}
+
+	this.processedContent = function processedContent() {
+		// return this.content.toLowerCase();
+		return this.processor(this.content);
+	}
+
+	this.palindrome = function palindrome() {
+		return this.processedContent() === this.processedContent().reverse();
+	}
+}
+
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	// Returns translation processed for palindrome testing
+	this.processedContent = function processedContent() {
+		// return this.translation.toLowerCase();
+		return this.processor(this.translation);
+	}
+}
+TranslatedPhrase.prototype = new Phrase();
